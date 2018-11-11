433fe00217d01644df2bdd9e3b9081f72494cbf1
Luis <lumart99@hotmail.com>
2018-11-10 20:33:13 -0500

    Updated the Look

--- Calendar App/Calendar App/Window.Designer.cs
+++ Calendar App/Calendar App/Window.Designer.cs
@@ -42,6 +42,9 @@
             // 
             // monthChooser
             // 
+            this.monthChooser.BackColor = System.Drawing.Color.White;
+            this.monthChooser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.monthChooser.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.monthChooser.ForeColor = System.Drawing.SystemColors.WindowText;
             this.monthChooser.FormattingEnabled = true;
             this.monthChooser.Items.AddRange(new object[] {
@@ -56,23 +59,27 @@
             "October",
             "November",
             "December"});
-            this.monthChooser.Location = new System.Drawing.Point(73, 12);
+            this.monthChooser.Location = new System.Drawing.Point(123, 26);
             this.monthChooser.Name = "monthChooser";
-            this.monthChooser.Size = new System.Drawing.Size(121, 24);
+            this.monthChooser.Size = new System.Drawing.Size(177, 39);
             this.monthChooser.TabIndex = 1;
             this.monthChooser.SelectedIndexChanged += new System.EventHandler(this.monthChooser_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.Size = new System.Drawing.Size(94, 32);
             this.label1.TabIndex = 2;
             this.label1.Text = "Month";
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Silver;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.tableLayoutPanel1.ColumnCount = 7;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28568F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28567F));
@@ -81,6 +88,7 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28567F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28567F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28596F));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.Color.White;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 142);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
@@ -96,6 +104,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(16, 119);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 17);
@@ -105,6 +114,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(109, 119);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 17);
@@ -114,6 +124,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(203, 119);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(63, 17);
@@ -124,6 +135,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(291, 119);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(83, 17);
@@ -133,6 +145,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(384, 119);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(68, 17);
@@ -142,6 +155,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
             this.label7.Location = new System.Drawing.Point(476, 119);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(47, 17);
@@ -151,6 +165,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.White;
             this.label8.Location = new System.Drawing.Point(567, 119);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(65, 17);
@@ -161,6 +176,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(666, 565);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -173,7 +189,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.monthChooser);
             this.Name = "Window";
-            this.Text = "Form1";
+            this.Text = "Calendar";
             this.ResumeLayout(false);
             this.PerformLayout();
 
